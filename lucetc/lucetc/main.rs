8611afb1753f70259446af1bdb2bd83738ee5704
Adam C. Foltzer <acfoltzer@fastly.com>
2019-10-30 16:45:28 -0700

    [lucetc] add `native` and `sandybridge` CPU profiles
    This required some reworking of how `CpuFeatures` is represented.

--- lucetc/lucetc/main.rs
+++ lucetc/lucetc/main.rs
@@ -53,7 +53,7 @@ pub fn run(opts: &Options) -> Result<(), Error> {
     let mut c = Lucetc::new(PathBuf::from(input))
         .with_bindings(bindings)
         .with_opt_level(opts.opt_level)
-        .with_cpu_features(opts.cpu_features);
+        .with_cpu_features(opts.cpu_features.clone());
 
     if let Some(ref builtins) = opts.builtins_path {
         c.builtins(builtins);
